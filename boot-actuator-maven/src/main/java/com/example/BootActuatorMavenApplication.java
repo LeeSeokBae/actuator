0d0ffd2ada4d6029ca73d5c992b30c5ebdd3b97f
seokbae <seokbae@danalssocio.com>
2017-08-28 13:48:34 +0900

    최초

---
+++ boot-actuator-maven/src/main/java/com/example/BootActuatorMavenApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootActuatorMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootActuatorMavenApplication.class, args);
+	}
+}
